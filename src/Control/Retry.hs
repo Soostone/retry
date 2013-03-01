9aaeebed6dd523ce25f8a5920cbb5fbb3698089a
Ozgun Ataman <ozataman@gmail.com>
2013-02-28 22:12:17 -0500

    Refactor some common code

--- src/Control/Retry.hs
+++ src/Control/Retry.hs
@@ -22,7 +22,19 @@
 ----------------------------------------------------------------------------
 
 
-module Control.Retry where
+module Control.Retry
+    (
+      -- * High Level Operation
+      RetrySettings (..)
+    , retrying
+    , recovering
+    , recoverAll
+
+    -- * Utilities
+    , delay
+    , flatDelay
+    , backoffDelay
+    ) where
 
 -------------------------------------------------------------------------------
 import           Control.Concurrent
@@ -35,8 +47,8 @@ import           Prelude               hiding (catch)
 -------------------------------------------------------------------------------
 
 
--- | Settings for retry behavior. Just using 'def' for default values.
--- should work in most cases.
+-- | Settings for retry behavior. Simply using 'def' for default
+-- values should work in most cases.
 data RetrySettings = RetrySettings {
       numRetries :: Int
     -- ^ Number of retries. Defaults to 5.
@@ -54,6 +66,19 @@ instance Default RetrySettings where
     def = RetrySettings 5 True 50
 
 
+-- | Delay thread using backoff delay for the nth retry.
+backoffDelay :: (Integral b, MonadIO m) => RetrySettings -> b -> m ()
+backoffDelay set@RetrySettings{..} !n = liftIO (threadDelay (2^n * delay set))
+
+-- | Delay thread using flat delay
+flatDelay :: MonadIO m => RetrySettings -> t -> m ()
+flatDelay set@RetrySettings{..} !n = liftIO (threadDelay $ delay set)
+
+-- | Delay in micro seconds
+delay :: RetrySettings -> Int
+delay RetrySettings{..} = baseDelay * 1000
+
+
 -- | Retry combinator for actions that don't raise exceptions, but
 -- signal in their type the outcome has failed. Examples are the
 -- 'Maybe', 'Either' and 'EitherT' monads.
@@ -65,18 +90,19 @@ retrying :: MonadIO m
          -> m b
          -- ^ Action to run
          -> m b
-retrying RetrySettings{..} chk f = go 0
+retrying set@RetrySettings{..} chk f = go 0
     where
-      delay = baseDelay * 1000
-      backoffRetry !n = liftIO (threadDelay (2^n * delay)) >> (go $! (n+1))
-      flatRetry !n = liftIO (threadDelay delay) >> (go $! (n+1))
       go n = do
           res <- f
           case chk res of
             True ->
               case n >= numRetries of
                 True -> return res
-                False -> if backoff then backoffRetry n else flatRetry n
+                False -> do
+                    if backoff
+                      then backoffDelay set n
+                      else flatDelay set n
+                    go $! n+1
             False -> return res
 
 
@@ -96,20 +122,15 @@ recoverAll set f = recovering set [h] f
 -- retrying the action a number of times.
 recovering :: forall m a. MonadCatchIO m
            => RetrySettings
-           -- ^ For default settings, just use 'def'
+           -- ^ Just use 'def' faor default settings
            -> [Handler m Bool]
            -- ^ Should a given exception be retried? Action will be
            -- retried if this returns True.
            -> m a
            -- ^ Action to perform
            -> m a
-recovering RetrySettings{..} hs f = go 0
+recovering set@RetrySettings{..} hs f = go 0
     where
-      delay = baseDelay * 1000
-      backoffRetry !n = liftIO (threadDelay (2^n * delay)) >> go (n+1)
-      flatRetry !n = liftIO (threadDelay delay) >> go (n+1)
-
-
       -- | Convert a (e -> m Bool) handler into (e -> m a) so it can
       -- be wired into the 'catches' combinator.
       transHandler :: Int -> Handler m Bool -> Handler m a
@@ -119,7 +140,11 @@ recovering RetrySettings{..} hs f = go 0
             True ->
               case n >= numRetries of
                 True -> throw e
-                False -> if backoff then backoffRetry n else flatRetry n
+                False -> do
+                    if backoff
+                      then backoffDelay set n
+                      else flatDelay set n
+                    go $! n+1
             False -> throw e
 
       -- handle :: forall e. Exception e => Handler m Bool -> Int -> e -> m a
