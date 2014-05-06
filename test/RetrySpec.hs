1741c716dc67f2a4514d36c5b601c0640adb5a9a
João Cristóvão <jmacristovao@gmail.com>
2014-05-06 17:07:12 +0100

    Cleaned up test code

--- test/RetrySpec.hs
+++ test/RetrySpec.hs
@@ -2,9 +2,7 @@
 
 module RetrySpec where
 
-import Control.Monad
 import Control.Applicative
-import Control.Concurrent (threadDelay)
 import Control.Retry
 import Control.Monad.Catch
 import Data.Time.Clock
@@ -14,7 +12,6 @@ import Test.Hspec.QuickCheck
 import Test.QuickCheck
 import Test.QuickCheck.Monadic as QCM
 import System.IO.Error
-import Control.Exception (IOException)
 
 isLeftAnd :: (a -> Bool) -> Either a b -> Bool
 isLeftAnd f ei = case ei of
@@ -28,13 +25,13 @@ spec = parallel $ describe "retry" $ do
 
   it "recovering test without quadratic retry delay"
      . property . monadicIO $ do
-    startTime <- run $ getCurrentTime
+    startTime <- run getCurrentTime
     timeout <- (+2) . getSmall . getPositive <$> pick arbitrary
     retries <- getSmall . getPositive <$> pick arbitrary
     res <- run . try $ recovering (RetrySettings (RLimit retries) False timeout)
-                              [Handler (\(e::SomeException) -> return True)]
+                              [Handler (\(_::SomeException) -> return True)]
                               (throwM (userError "booo"))
-    endTime <- run $ getCurrentTime
+    endTime <- run getCurrentTime
     QCM.assert (isLeftAnd isUserError res)
     let ms' = ((fromInteger . toInteger $ (timeout * retries)) / 1000.0)
     QCM.assert (diffUTCTime endTime startTime >= ms')
