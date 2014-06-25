8e65d8db1f86d0bf2fdfbfd09530b000494c0658
Ozgun Ataman <ozgun.ataman@soostone.com>
2014-06-25 16:04:28 -0400

    Get tests working with latest API and switch to microseconds for speed

--- test/QuadraticDelayRetrySpec.hs
+++ test/QuadraticDelayRetrySpec.hs
@@ -2,16 +2,16 @@
 
 module QuadraticDelayRetrySpec where
 
-import Control.Applicative
-import Control.Retry
-import Control.Monad.Catch
-import Data.Time.Clock
-import Data.Time.LocalTime ()
-import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck
-import Test.QuickCheck.Monadic as QCM
-import System.IO.Error
+import           Control.Applicative
+import           Control.Monad.Catch
+import           Control.Retry
+import           Data.Time.Clock
+import           Data.Time.LocalTime     ()
+import           System.IO.Error
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+import           Test.QuickCheck.Monadic as QCM
 
 isLeftAnd :: (a -> Bool) -> Either a b -> Bool
 isLeftAnd f ei = case ei of
@@ -27,12 +27,12 @@ spec = parallel $ describe "quadratic delay" $ do
     startTime <- run getCurrentTime
     timeout <- pick . choose $ (0,15)
     retries <- pick . choose $ (0,8)
-    res <- run . try $ recovering (RetrySettings (RLimit retries) True timeout)
+    res <- run . try $ recovering (exponentialBackoff timeout <> limitRetries retries)
                               [Handler (\(_::SomeException) -> return True)]
                               (throwM (userError "booo"))
     endTime <- run getCurrentTime
     QCM.assert (isLeftAnd isUserError res)
     let tmo = if retries > 0 then timeout * 2 ^ (retries - 1) else 0
-    let ms' = ((fromInteger . toInteger $ tmo) / 1000.0)
+    let ms' = ((fromInteger . toInteger $ tmo) / 1000000.0)
     QCM.assert (diffUTCTime endTime startTime >= ms')
 
--- test/RetrySpec.hs
+++ test/RetrySpec.hs
@@ -2,16 +2,16 @@
 
 module RetrySpec where
 
-import Control.Applicative
-import Control.Retry
-import Control.Monad.Catch
-import Data.Time.Clock
-import Data.Time.LocalTime ()
-import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck
-import Test.QuickCheck.Monadic as QCM
-import System.IO.Error
+import           Control.Applicative
+import           Control.Monad.Catch
+import           Control.Retry
+import           Data.Time.Clock
+import           Data.Time.LocalTime     ()
+import           System.IO.Error
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+import           Test.QuickCheck.Monadic as QCM
 
 isLeftAnd :: (a -> Bool) -> Either a b -> Bool
 isLeftAnd f ei = case ei of
@@ -28,12 +28,12 @@ spec = parallel $ describe "retry" $ do
     startTime <- run getCurrentTime
     timeout <- pick . choose $ (0,15)
     retries <- getSmall . getPositive <$> pick arbitrary
-    res <- run . try $ recovering (RetrySettings (RLimit retries) False timeout)
+    res <- run . try $ recovering (constantDelay timeout <> limitRetries retries)
                               [Handler (\(_::SomeException) -> return True)]
                               (throwM (userError "booo"))
     endTime <- run getCurrentTime
     QCM.assert (isLeftAnd isUserError res)
-    let ms' = ((fromInteger . toInteger $ (timeout * retries)) / 1000.0)
+    let ms' = (fromInteger . toInteger $ (timeout * retries)) / 1000000.0
     QCM.assert (diffUTCTime endTime startTime >= ms')
 
 
