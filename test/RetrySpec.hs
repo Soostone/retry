8547e172ad57dd5176987d562fde12cd62bbbef0
Mauricio Scheffer <mauricioscheffer@gmail.com>
2014-08-29 21:09:13 -0300

    Test monoid laws for RetryPolicy

--- test/RetrySpec.hs
+++ test/RetrySpec.hs
@@ -2,6 +2,7 @@
 
 module RetrySpec where
 
+import           Data.Monoid
 import           Control.Applicative
 import           Control.Monad.Catch
 import           Control.Retry
@@ -12,12 +13,12 @@ import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 import           Test.QuickCheck.Monadic as QCM
+import           Test.QuickCheck.Function (apply)
 
 isLeftAnd :: (a -> Bool) -> Either a b -> Bool
 isLeftAnd f ei = case ei of
   Left v -> f v
   _      -> False
-
 
 {-# ANN spec ("HLint: ignore Redundant do"::String) #-}
 spec :: Spec
@@ -36,4 +37,22 @@ spec = parallel $ describe "retry" $ do
     let ms' = (fromInteger . toInteger $ (timeout * retries)) / 1000000.0
     QCM.assert (diffUTCTime endTime startTime >= ms')
 
+  describe "Policy is a monoid" $ do
+    let toPolicy = RetryPolicy . apply
+    let prop left right  =
+          property $ \a x -> 
+                      let applyPolicy f = getRetryPolicy (f $ toPolicy a) x in
+                      applyPolicy left == applyPolicy right
+    let prop3 left right  =
+          property $ \a b c x -> 
+                      let applyPolicy f = getRetryPolicy (f (toPolicy a) (toPolicy b) (toPolicy c)) x in
+                      applyPolicy left == applyPolicy right
 
+    it "left identity" $
+      prop (\p -> mempty <> p) id
+
+    it "right identity" $
+      prop (\p -> p <> mempty) id
+
+    it "associativity" $
+      prop3 (\x y z -> x <> (y <> z)) (\x y z -> (x <> y) <> z)
